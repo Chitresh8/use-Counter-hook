4537f3e6cb4d324abb312b532599f714db66deff
Chitresh8 <chitreshbabu8@gmail.com>
2023-12-07 07:00:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import BackwardCounter from './components/BackwardCounter';
-import ForwardCounter from './components/ForwardCounter';
+import React from "react";
+import "./App.css";
+import BackwardCounter from "./components/BackwardCounter";
+import ForwardCounter from "./components/ForwardCounter";
 
 function App() {
   return (
     <React.Fragment>
+      <h1 className="text-center">Counters with use Counter Custom hook</h1>
       <ForwardCounter />
       <BackwardCounter />
     </React.Fragment>
